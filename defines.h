5bb4021e91c6a6963d2cfe40571baf27e0551c99
Liana Duenha <lianaduenha@gmail.com>
2015-05-04 15:24:09 -0400

    Update Makefile.rules and defines.h

--- defines.h
+++ defines.h
@@ -27,26 +27,27 @@
 #define MAX_WORKERS 64 	/* This is a arbitrary limit for the number of processors. If necessary, this value can be modified, but
  there is no guarantee that all applications will work properly */
 
-// NOC STATIC PARAMETERS
-#define BASE_MEMORY_ADDRESS 536870912
-#define DELTA 16777216  // 16^6 = 0x1000000
+// NOC and Router STATIC PARAMETERS
 #define MAX_LINES 72
 #define MAX_COLUMNS 3 
 #define ON 1
 #define OFF 0
 #define SIZE_OF_BUFFER_PACKAGES 2000
 
-
-// DVFS STATIC PARAMETERS
-// The definition of the power states depends on how the software (application) will explore this feature
-// By default, the acPthread will use just the states LOW and HIGH. So, if you want to enable DVFS with this two 
-// power states, you must define the "id" that you want for each one of this states (based on the power table that you will use 
-// see processor/mips/ac_power_stats.H or processors/sparc/ac_power_stats.H to know more about this)
-// In case you define different power states here, you must also update the acPthread library to use them (sw/acPthread.c).
-
-// But if you want to desable DFS (using just a single power state), use HIGH=LOW=0 
-#define HIGH 0
-#define LOW 0
+ // These parameters define the address space for IPs
+// We consider that memory address space is from 0 to 0x20000000-1
+// The address to access the Lock IP is 0x20000000
+// The address to access the DVFS IP is 0x21000000
+// The address to access the INTR_CTRL IP is 0x22000000
+// Next, we use a DELTA_IP_ADDRESS = 0x100 as a default value
+// If you want to update these values, be carefull maintaining the coherency with the tlm_router, tlm_noc_at and tlm_noc_lt
+// routing definitions (see is/tlm_router, is/tlm_noc_lt, is/tlm_noc_at)
+#define BASE_MEMORY_ADDRESS 536870912
+#define LOCK_ADDRESS       0x20000000
+#define INTR_CTRL_ADDRESS  0X21000000
+#define DVFS_ADDRESS	   0x22000000
+#define DELTA_IP_ADDRESS   0X00000100
+#define DELTA 16777216  // 16^6 = 0x1000000
 
 
 
@@ -56,19 +57,9 @@
 #define MEMORY_DEBUG 0
 #define ROUTER_DEBUG 0
 #define DVFS_DEBUG 0
+#define INTR_CTRL_DEBUG 0
 
 // ROUTER STATIC PARAMETERS
-// These parameters define the address space for IPs
-// We consider that memory address space is from 0 to 0x20000000-1
-// The address to access the Lock IP is 0x20000000
-// The address to access the DVFS IP is 0x21000000
-// Next, we use a DELTA_IP_ADDRESS = 0x100 as a default value
-// If you want to update these values, be carefull maintaining the coherency with the tlm_router, tlm_noc_at and tlm_noc_lt
-// routing definitions (see is/tlm_router, is/tlm_noc_lt, is/tlm_noc_at)
-#define LOCK_ADDRESS       0x20000000
-#define DVFS_ADDRESS	   0x21000000
-#define DELTA_IP_ADDRESS   0X00000100
-
 
 // TLM2 PAYLOAD STATIC PARAMETERS
 // Define the transaction direction
@@ -82,6 +73,21 @@
 #define TIME_ROUTER 2
 #define TIME_LOCK 1
 #define TIME_DVFS 1
+
+
+
+// DVFS STATIC PARAMETERS
+// The definition of the power states depends on how the software (application) will explore this feature
+// By default, the acPthread will use just the states LOW and HIGH. So, if you want to enable DVFS with this two 
+// power states, you must define the "id" that you want for each one of this states (based on the power table that you will use 
+// see processor/mips/ac_power_stats.H or processors/sparc/ac_power_stats.H to know more about this)
+// In case you define different power states here, you must also update the acPthread library to use them (sw/acPthread.c).
+
+// But if you want to desable DFS (using just a single power state), use HIGH=LOW=0 
+
+//#define OFF // already defined for NoCs
+#define HIGH 3
+#define LOW 0
 
 
 #ifdef PROCMIPS 
@@ -108,10 +114,11 @@
 	#define PROCESSOR_NAME_parms arm_parms
 #endif 
 
-
 // NoC Hermes STATIC PARAMETERS - NOT IN USE IN THIS VERSION
 #define LOCAL_MEM_SIZE  67108864
 #define TEXTAREA	0x00d16f
  
+
+//#define CACHE_POWER_TABLE_FILE "table_cache_8k_2w_45nm.csv"
 
 #endif
