c2fa561788bb29ef1402c15b01d09b50aec1dfa1
liana <lianaduenha@gmail.com>
2015-10-09 08:56:31 -0400

    [FIX]: Starting ALL processors in ON mode. We will keep all processors in ON mode until we find synchronization bugs that are causing problems in the NoC-AT simulations with ON/OFF interruptions.

--- platforms/platform.router.lt/main.cpp
+++ platforms/platform.router.lt/main.cpp
@@ -137,7 +137,9 @@ int sc_main(int ac, char *av[])
 	// Processor 0 starts simulatino in ON-mode while the other processors are in OFF-mode
 	for (int i=1; i<N_WORKERS; i++)
     {
-        intr_ctrl.send(i,ON); // turn off processors 1,..,N_WORKERS-1
+        //intr_ctrl.send(i,OFF);// turn off processors 1,..,N_WORKERS-1
+        intr_ctrl.send(i,ON); 
+
     }
     intr_ctrl.send(0,ON);    // turn on processor 0 (master)
 
